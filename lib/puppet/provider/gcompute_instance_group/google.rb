b041e91f667c08057ad4903d71739c1cc561fd5d
Alex Stephen <alexstephen@google.com>
2018-08-13 23:20:49 +0000

    Object.input should == noneditable

--- lib/puppet/provider/gcompute_instance_group/google.rb
+++ lib/puppet/provider/gcompute_instance_group/google.rb
@@ -115,7 +115,11 @@ Puppet::Type.type(:gcompute_instance_group).provide(:google) do
     debug('flush')
     # return on !@dirty is for aiding testing (puppet already guarantees that)
     return if @created || @deleted || !@dirty
-    raise 'InstanceGroup cannot be edited.'
+    update_req = Google::Compute::Network::Put.new(self_link(@resource),
+                                                   fetch_auth(@resource),
+                                                   'application/json',
+                                                   resource_to_request)
+    @fetched = wait_for_operation update_req.send, @resource
   end
 
   def dirty(field, from, to)
